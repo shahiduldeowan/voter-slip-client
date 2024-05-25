2c4e7526cb3ac8ced4e55f5d55b37f341d42c9da
Shahidul Deowan <deowanshahidul@gmail.com>
2024-05-25 19:40:48 +0600

    create view route for view related elements

---
+++ src/routes/ViewerRoute.jsx
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+import { Navigate } from "react-router-dom";
+import LoadingSpinner from "../components/Shared/LoadingSpinner";
+import useAuth from "../hooks/useAuth";
+
+const ViewerRoute = ({ children }) => {
+  const { user, isLoading } = useAuth();
+
+  if (isLoading) return <LoadingSpinner />;
+  if (user?.RoleName === "Viewer") return children;
+  return <Navigate to="/not-found" />;
+};
+
+ViewerRoute.propTypes = {
+  children: PropTypes.element,
+};
+
+export default ViewerRoute;
