381e9233408cdd88ce03ea38e94b1bb4782149c4
Shahidul Deowan <deowanshahidul@gmail.com>
2024-05-27 14:18:13 +0600

    add constants.js file to declare constant string

---
+++ src/config/constants.js
@@ -0,0 +1,14 @@
+const UPLOAD_FILES_STATUS = {
+  SELECT: "select",
+  UPLOADING: "uploading",
+  DONE: "done",
+};
+
+const UPLOAD_FILES_TYPE = {
+  XLSX: "xlsx",
+  JPG: "jpg",
+  JPEG: "jpeg",
+  PNG: "png",
+};
+
+export { UPLOAD_FILES_STATUS, UPLOAD_FILES_TYPE };
