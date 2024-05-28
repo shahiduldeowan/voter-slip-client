9afec71ce94578b95e3be40e3610ee8dd94e6de2
Shahidul Deowan <deowanshahidul@gmail.com>
2024-05-28 13:58:21 +0600

    declare layout dashboard

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,5 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Dashboard/Sidebar/Sidebar";
+
 const Dashboard = () => {
-  return <div>Dashboard layout</div>;
+  return (
+    <div className="relative min-h-screen md:flex">
+      <Sidebar />
+      <div className="flex-1">
+        <div className="p-5 h-[100vh] w-full">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
