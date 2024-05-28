16256fcbacfad26dff3b19e28c0182c89e9e9079
Shahidul Deowan <deowanshahidul@gmail.com>
2024-05-28 13:54:31 +0600

    add MenuItem component for dashboard sidebar

---
+++ src/components/Dashboard/Sidebar/Menu/MenuItem.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
+
+const MenuItem = ({ label, address, icon: Icon }) => {
+  return (
+    <NavLink
+      to={address}
+      end
+      className={({ isActive }) =>
+        `${
+          isActive ? "bg-app-primary text-gray-50 dark:text-gray-900" : ""
+        } flex items-center p-2 space-x-3 rounded-md`
+      }
+    >
+      <Icon className="w-5 h-5 fill-current text-gray-400 dark:text-gray-600" />
+      <span>{label}</span>
+    </NavLink>
+  );
+};
+
+MenuItem.propTypes = {
+  label: PropTypes.string.isRequired,
+  address: PropTypes.string.isRequired,
+  icon: PropTypes.elementType.isRequired,
+};
+
+export default MenuItem;
