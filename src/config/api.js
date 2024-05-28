700f20daef4b95d266591e91274b1f6b0b3f4a6e
Shahidul Deowan <deowanshahidul@gmail.com>
2024-05-28 13:56:18 +0600

    add AUTH_STATUS, MEMBERS_EXCEL_FILE, GET_MEMBERS property for API_ENDPOINT object

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,9 @@
 const API_ENDPOINT = {
   USER_LOGIN: "/api/v1/user/login",
   USER_LOGOUT: "/api/v1/user/logout",
+  AUTH_STATUS: "/api/v1/user/auth-status",
+  MEMBERS_EXCEL_FILE: "/api/v1/member/upload",
+  GET_MEMBERS: "/api/v1/member",
 };
 
 export { API_ENDPOINT };
